f72e050531d7c7f08ba12abeedcb8f1480a1b7a1
Nagara86 <114559845+Nagara86@users.noreply.github.com>
2025-01-11 12:46:31 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 echo "SCRIPT AUTO INSTALL WINDOWS by HIDESSH"
 echo
 echo "Pilih OS yang ingin anda install"
-echo "[1] Windows 2019(Default)"
+echo "[1] Windows 2022 (Default)"
 echo "[2] Windows 2016"
 echo "[3] Windows 2012"
 echo "[4] Windows 10"
@@ -11,12 +11,12 @@ echo "[5] Chat Ryan Untuk Add OS lain"
 read -p "Pilih [1]: " PILIHOS
 
 case "$PILIHOS" in
-    1|"") PILIHOS="https://download1322.mediafire.com/q45q3lnfq46gmouZIGTp712CYsj2ZuIMho3b7Z-7A2TIBmrDCLiePiRXdKLeYvaXkwaIRHaN3UaEiDMbE3npFuMTAxRoP6Iu0tCKma3xJmWL_v1wcLngELHu78oqJ-OJGwSl87JkZzjUKxOLnxYR6mBUMo6-0jfbb2xg8zKnlj8SJA/s92phcj6bgp0yhg/Windows2022.gz";;
-    2) PILIHOS="https://download1322.mediafire.com/q45q3lnfq46gmouZIGTp712CYsj2ZuIMho3b7Z-7A2TIBmrDCLiePiRXdKLeYvaXkwaIRHaN3UaEiDMbE3npFuMTAxRoP6Iu0tCKma3xJmWL_v1wcLngELHu78oqJ-OJGwSl87JkZzjUKxOLnxYR6mBUMo6-0jfbb2xg8zKnlj8SJA/s92phcj6bgp0yhg/Windows2022.gz";;
-    3) PILIHOS="https://download1322.mediafire.com/q45q3lnfq46gmouZIGTp712CYsj2ZuIMho3b7Z-7A2TIBmrDCLiePiRXdKLeYvaXkwaIRHaN3UaEiDMbE3npFuMTAxRoP6Iu0tCKma3xJmWL_v1wcLngELHu78oqJ-OJGwSl87JkZzjUKxOLnxYR6mBUMo6-0jfbb2xg8zKnlj8SJA/s92phcj6bgp0yhg/Windows2022.gz";;
-    4) PILIHOS="https://download1322.mediafire.com/q45q3lnfq46gmouZIGTp712CYsj2ZuIMho3b7Z-7A2TIBmrDCLiePiRXdKLeYvaXkwaIRHaN3UaEiDMbE3npFuMTAxRoP6Iu0tCKma3xJmWL_v1wcLngELHu78oqJ-OJGwSl87JkZzjUKxOLnxYR6mBUMo6-0jfbb2xg8zKnlj8SJA/s92phcj6bgp0yhg/Windows2022.gz";;
-    5) read -p "[?] Masukkan Link GZ mu : " PILIHOS;;
-    *) echo "[!] Pilihan salah"; exit;;
+	1|"") PILIHOS="https://download1322.mediafire.com/q45q3lnfq46gmouZIGTp712CYsj2ZuIMho3b7Z-7A2TIBmrDCLiePiRXdKLeYvaXkwaIRHaN3UaEiDMbE3npFuMTAxRoP6Iu0tCKma3xJmWL_v1wcLngELHu78oqJ-OJGwSl87JkZzjUKxOLnxYR6mBUMo6-0jfbb2xg8zKnlj8SJA/s92phcj6bgp0yhg/Windows2022.gz";;
+	2) PILIHOS="https://file.nixpoin.com/windows2016.gz";;
+	3) PILIHOS="https://file.nixpoin.com/windows2012v2.gz";;
+	4) PILIHOS="https://file.nixpoin.com/win10.gz";;
+	5) read -p "[?] Masukkan Link GZ mu : " PILIHOS;;
+	*) echo "[!] Pilihan salah"; exit;;
 esac
 
 echo "[*] Password yang saya buat sudah masuk wordlist bruteforce, silahkan masukkan password yang lebih aman!"
@@ -35,7 +35,6 @@ del /f /q "%temp%\Admin.vbs"
 exit /b 2)
 net user Administrator $PASSADMIN
 
-
 for /f "tokens=3*" %%i in ('netsh interface show interface ^|findstr /I /R "Local.* Ethernet Ins*"') do (set InterfaceName=%%j)
 netsh -c interface ip set address name="Ethernet Instance 0" source=static address=$IP4 mask=255.255.240.0 gateway=$GW
 netsh -c interface ip add dnsservers name="Ethernet Instance 0" address=8.8.8.8 index=1 validate=no
@@ -45,7 +44,6 @@ cd /d "%ProgramData%/Microsoft/Windows/Start Menu/Programs/Startup"
 del /f /q net.bat
 exit
 EOF
-
 
 cat >/tmp/dpart.bat<<EOF
 @ECHO OFF
